5f8d521f156a9a1f670f8bba82248a77ed7eeb8c
ElJoker63 <eljoker630@gmail.com>
2025-02-15 19:05:03 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 ADB = 'adb-fastboot'
 PARTITION = 'partition'
 PAT = 'copy-partitions-20220613-signed.zip'
-GAPPS = 'NikGapps-stock-arm64-14-20241104-signed.zip'
+GAPPS = 'NikGapps-stock-arm64-15-20241104-signed.zip'
 MAGISK = 'Magisk-v28.0.apk'
 REPO_MAGISK = 'https://api.github.com/repos/topjohnwu/Magisk/releases/latest'
 TWRPURL = 'https://dl.twrp.me/lake/twrp-3.7.0_9-0-lake.img'
